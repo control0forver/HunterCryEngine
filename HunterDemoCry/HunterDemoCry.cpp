4086a44c8f57b83561789d380f742a993cfd3167
To ling <sjzbjs@outlook.com>
2023-04-28 21:45:47 +0800

    code

---
+++ HunterDemoCry/HunterDemoCry.cpp
@@ -0,0 +1,31 @@
+﻿// HunterDemoCry.cpp : A Demo of Cry Game Using Hunter Cry Engine
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#pragma comment(lib, "HunterCryEngine.lib")
+#include "HunterCryEngine/HunterCry.h"
+
+int RunGame();
+
+int main(int argc, char** argv)
+{
+    int iResult = RunGame();
+
+    printf("Cry Finished: Returns %d\n", iResult);
+    printf("Press Enter to Continue.\n");
+    int _i = getchar();
+
+    exit(iResult);
+}
+
+int RunGame()
+{
+    HunterCry engine;
+    HunterCry engine1;
+
+    printf("Engines: %d\n", HunterCry::GetEnginesCount());
+
+    return 0;
+}
